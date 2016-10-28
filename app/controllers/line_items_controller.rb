ed1965911c45020794e6dfc6f1f349a34d6e13ba
bartekz94 <b.zaucha1@nuigalway.ie>
2016-10-28 15:30:12 +0100

    added quantity column to line items table

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -26,8 +26,8 @@ class LineItemsController < ApplicationController
   # POST /line_items
   # POST /line_items.json
   def create
-    @product = Product.find(params[:product_id])
-    @line_item = @cart.line_items.build(product: @product)
+    product = Product.find(params[:product_id])
+    @line_item = @cart.add_product(product.id)
 
     respond_to do |format|
       if @line_item.save
